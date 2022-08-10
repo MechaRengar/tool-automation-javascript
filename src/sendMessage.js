19b0c908ed01d57b960600dfd2a52f9aec853c3e
VietPT8 <vietkaka9x@gmail.com>
2022-08-10 13:21:09 +0700

    check account git

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -1,27 +1,27 @@
-const puppeteer=require('puppeteer');
-require('dotenv').config()
+const puppeteer = require('puppeteer');
+require('dotenv').config();
 
 const username = process.env.USER;
 const password = process.env.PASS;
 const Việt = process.env.Viet;
-const Thuỳ = process.env.Thuy;
-(async()=> {
+// const Thuỳ = process.env.Thuy;
+(async () => {
   let i = 1;
-  const browser = await puppeteer.launch({headless: true});
-  const page=await browser.newPage();
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
   await page.goto('https://m.facebook.com');
-  await	page.type('#m_login_email',username);
-  await	page.type('#m_login_password',password);
-  await	page.click("button[name='login']");
+  await page.type('#m_login_email', username);
+  await page.type('#m_login_password', password);
+  await page.click("button[name='login']");
   // đợi trang tải xong
-  await	page.waitForNavigation();
+  await page.waitForNavigation();
   await page.goto(`https://m.facebook.com/messages/thread/${Việt}/`);
-  while(i < 1001) {
-  await page.type('#composerInput', `${i}. trộm vía\n`);
-  if(i%50 == 0) {
-    await page.click("button[name='send']");
-  }
-      i++;
+  while (i < 1001) {
+    await page.type('#composerInput', `${i}. trộm vía\n`);
+    if (i % 50 == 0) {
+      await page.click("button[name='send']");
+    }
+    i++;
   }
   console.log('Done!');
   await browser.close();
